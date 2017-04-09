7cea30d7c53742e066a61656c6da4d6797c00ec5
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-04-09 16:39:21 +0200

    image logic

--- glitchappcore/src/main/java/me/bemind/glitchappcore/ImageLogic.kt
+++ glitchappcore/src/main/java/me/bemind/glitchappcore/ImageLogic.kt
@@ -1,8 +1,23 @@
 package me.bemind.glitchappcore
 
+import android.content.Context
+import android.graphics.Bitmap
+import com.github.oliveiradev.lib.RxPhoto
+import com.github.oliveiradev.lib.shared.TypeRequest
+import rx.Observable
+
 /**
  * Created by angelomoroni on 04/04/17.
  */
 interface IImageLogic{
+
+    fun getImage(context: Context,
+                 type:TypeRequest = TypeRequest.GALLERY) : Observable<Bitmap>
+}
+
+class ImageLogic : IImageLogic{
+    override fun getImage(context: Context, type: TypeRequest): Observable<Bitmap> {
+        return RxPhoto.requestBitmap(context, type)
+    }
 
 }
