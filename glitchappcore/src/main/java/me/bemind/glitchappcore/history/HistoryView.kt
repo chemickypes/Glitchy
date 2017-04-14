82628818cf60ce1483962fd2c31db550545e1578
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-04-14 11:14:10 +0200

    simple funciont in view interface

--- glitchappcore/src/main/java/me/bemind/glitchappcore/history/HistoryView.kt
+++ glitchappcore/src/main/java/me/bemind/glitchappcore/history/HistoryView.kt
@@ -3,3 +3,12 @@ package me.bemind.glitchappcore.history
 /**
  * Created by angelomoroni on 14/04/17.
  */
+
+interface IHistoryView {
+
+    fun canBack()
+
+    fun back()
+
+    fun addImage()
+}
