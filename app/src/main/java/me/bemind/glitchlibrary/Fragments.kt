0c7b9a978f1f2162d0e71455cf9d868706b11b9e
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-05-03 17:04:56 +0200

    removed butterknife simple layout of menu fragment

--- app/src/main/java/me/bemind/glitchlibrary/Fragments.kt
+++ app/src/main/java/me/bemind/glitchlibrary/Fragments.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import org.jraf.android.alibglitch.GlitchEffect
 
 /**
  * Created by angelomoroni on 03/05/17.
@@ -12,7 +14,21 @@ import android.view.ViewGroup
 
 class MenuFragment: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    var rootview : View? = null
+
+    val logoApp : ImageView? by lazy {
+        rootview?.findViewById(R.id.app_logo) as ImageView
     }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        rootview = inflater?.inflate(R.layout.menu_fragment,container)!!
+
+        logoApp?.setOnClickListener {
+            GlitchEffect.showGlitch(activity)
+        }
+        return rootview!!
+    }
+
+
+
 }
