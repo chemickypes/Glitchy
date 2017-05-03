2b6883e8d5b155c4448e914d51f4483b3f10db0e
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-05-03 15:04:10 +0200

    butterknife import for kotlin

---
+++ app/src/main/java/me/bemind/glitchlibrary/Fragments.kt
@@ -0,0 +1,18 @@
+package me.bemind.glitchlibrary
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by angelomoroni on 03/05/17.
+ */
+
+class MenuFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
