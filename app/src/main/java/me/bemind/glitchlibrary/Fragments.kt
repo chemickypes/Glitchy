6ce97c5501b656490f0a0fbba0bc52b02ed96818
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-05-03 17:14:24 +0200

    app version dynamically in the app

--- app/src/main/java/me/bemind/glitchlibrary/Fragments.kt
+++ app/src/main/java/me/bemind/glitchlibrary/Fragments.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import org.jraf.android.alibglitch.GlitchEffect
 
 /**
@@ -20,11 +21,20 @@ class MenuFragment: Fragment() {
         rootview?.findViewById(R.id.app_logo) as ImageView
     }
 
+    val versionApp : TextView by lazy {
+        rootview?.findViewById(R.id.version_text) as TextView
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         rootview = inflater?.inflate(R.layout.menu_fragment,container)!!
 
         logoApp?.setOnClickListener {
             GlitchEffect.showGlitch(activity)
+        }
+
+        val pInfo = activity.packageManager.getPackageInfo(activity.packageName,0)
+        versionApp.apply {
+            text = getString(R.string.v_d,pInfo.versionName)
         }
         return rootview!!
     }
