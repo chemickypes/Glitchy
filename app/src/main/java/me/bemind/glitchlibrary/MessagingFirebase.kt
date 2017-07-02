b3528bc0ef1aab7c9f3c7311ac87aeadfa05fb7c
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-07-02 11:14:24 +0200

    optimized imports

--- app/src/main/java/me/bemind/glitchlibrary/MessagingFirebase.kt
+++ app/src/main/java/me/bemind/glitchlibrary/MessagingFirebase.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceIdService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import me.bemind.glitchappcore.RetainedFragment.TAG
 import com.google.firebase.iid.FirebaseInstanceId
 
 
