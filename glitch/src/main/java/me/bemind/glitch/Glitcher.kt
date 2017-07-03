47b49a0095a7e5329bde69833f8dcf928f545fb8
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-07-03 21:22:01 +0200

    pixel effect algorithm

--- glitch/src/main/java/me/bemind/glitch/Glitcher.kt
+++ glitch/src/main/java/me/bemind/glitch/Glitcher.kt
@@ -5,6 +5,9 @@ import android.util.FloatMath
 import android.util.Log
 import java.io.ByteArrayOutputStream
 import java.util.*
+import android.R.attr.bitmap
+
+
 
 /**
  * Created by angelomoroni on 27/03/17.
@@ -627,6 +630,35 @@ object Glitcher {
 
         //canvas?.drawRect(0.0f, 0.0f, w.toFloat(), h.toFloat(),debossPaint)
         //canvas?.drawFilter
+    }
+
+    fun pixelCanvas(canvas: Canvas?,density: Int = 70){
+
+        val paint : Paint = Paint()
+
+        val cols : Double = if(density>1)density.toDouble() else 1.0
+        val blockSize : Double = w/cols
+        val rows : Double = Math.ceil(h/blockSize)
+        val startX = 0
+        val startY = 0
+
+        for(row in 0 until rows.toInt()){
+            for (col in 0 until cols.toInt()){
+                val pixelCoordX : Double = blockSize + col + startX
+                val pixelCoordY : Double = blockSize + row + startY
+
+                val midY = pixelCoordY + blockSize / 2
+                val midX = pixelCoordX + blockSize / 2
+
+                if(midX >= w || midX < 0) continue
+                if(midY >= h || midY < 0) continue
+
+                paint.color = baseBitmap?.getPixel(midX.toInt(),midY.toInt())?:0
+                canvas?.drawRect(pixelCoordX.toFloat(),pixelCoordY.toFloat(),
+                        (pixelCoordX+blockSize).toFloat(),(pixelCoordY+blockSize).toFloat(),paint)
+            }
+        }
+
     }
 
     private fun getJpegHeaderSize(byteArrayIn: ByteArray): Int {
