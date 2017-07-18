3aed46134b71b3281efb555f2d3f50adbcb56f98
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-07-18 21:41:56 +0200

    model

--- glitch/src/main/java/me/bemind/glitch/Models.kt
+++ glitch/src/main/java/me/bemind/glitch/Models.kt
@@ -5,7 +5,7 @@ package me.bemind.glitch
  */
 
 enum class Effect {
-    BASE,ANAGLYPH, GLITCH, WEBP, SWAP,NOISE,GHOST,HOOLOOVOO,WOBBLE, PIXEL
+    BASE,ANAGLYPH, GLITCH, WEBP, SWAP,NOISE,GHOST,HOOLOOVOO,WOBBLE, PIXEL, TPIXEL
 }
 
 enum class TypeEffect {
